67b031ef9f93a3a66a9ed5b7fc76252759c5d790
Mansoor Faqiri <mzfaqiri@gmail.com>
2025-01-14 13:34:14 -0500

    seperated nvim config files

---
+++ nixos/nixosModules/nvim/languages.nix
@@ -0,0 +1,53 @@
+{ ... }: {
+
+programs.nvf.vim.languages = {
+
+  enableLSP = true;
+  enableFormat = true;
+  enableTreesitter = true;
+
+  assembly.enable = true;
+
+  bash.enable = true;
+
+  clang.enable = true;
+
+  csharp.enable = true;
+
+  css.enable = true;
+
+  dart = {
+    enable = true;
+
+    flutter-tools.enable = true;
+  };
+
+  go.enable = true;
+
+  html.enable = true;
+
+  java.enable = true;
+
+  lua.enable = true;
+
+  markdown = {
+    enable = true;
+
+    extensions.render-markdown-nvim.enable = true;
+  };
+
+  nix.enable = true;
+
+  php.enable = true;
+
+  python.enable = true;
+
+  rust.enable = true;
+
+  sql.enable = true;
+
+  tailwind.enable = true;
+
+  ts.enable = true;
+};
+}
---
+++ nixos/nixosModules/nvim/options.nix
@@ -0,0 +1,37 @@
+{ ... }: {
+  programs.nvf.vim.options = {
+          
+    guicursor = "";
+
+    nu = true;
+    relativenumber = true;
+
+    tabstop = 4;
+    softtabstop = 4;
+    shiftwidth = 4;
+    expandtab = true;
+
+    smartindent = true;
+
+    wrap = false;
+
+    swapfile = false;
+    backup = false;
+
+    undodir = "../../.vim/undodir";
+    undofile = true;
+
+    hlsearch = false;
+    incsearch = true;
+
+    termguicolors = true;
+
+    scrolloff = 8;
+    signcolumn = "yes";
+
+    updatetime = 50;
+
+    colorcolumn = "80";
+
+  };
+}
